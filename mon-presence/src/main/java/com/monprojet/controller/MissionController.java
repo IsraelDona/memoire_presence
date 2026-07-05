52cac33df8d93c452b11b27c3c8dd28a2883332d
ISRAEL BOSS <bossikpononisrael@gmail.com>
2026-07-05 02:09:06 +0100

    modification des fichiers

--- mon-presence/src/main/java/com/monprojet/controller/MissionController.java
+++
@@ -1,5 +0,0 @@
-package com.monprojet.controller;
-
-public class MissionController {
-
-}
--- mon-presence/src/main/java/com/monprojet/repository/MissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.monprojet.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.monprojet.entity.Mission;
-
-public interface MissionRepository
-        extends JpaRepository<Mission, Long> {
-}
